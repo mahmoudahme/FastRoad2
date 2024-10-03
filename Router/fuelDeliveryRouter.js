e90e09df508a5eb643ea22a80f5c16850f8f33ca
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-10-03 19:49:06 +0300

    Delete Router/fuelDeliveryRouter.js

--- Router/fuelDeliveryRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { CreateFuelDelivery, getAllFuelDelivery, getOneFuelDelivery } from "../Controller/fuelDeliverycontroller.js";
-
-const router = express.Router();
-
-router.post("/", CreateFuelDelivery);
-router.get("/", getAllFuelDelivery);
-router.get("/:FuelDelivery", getOneFuelDelivery);
-export default router;
