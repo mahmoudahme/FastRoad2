4abc3995552691b16c860281c08acc7b0e1baa76
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-10-03 19:44:27 +0300

    Delete Router directory

--- Router/lockoutRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express" ;
-import {CreateLockOut , getAllLockout , getOneLockOut} from "../Controller/lockoutController.js"
-
-const router = express.Router() ;
-
-router.post("/" , CreateLockOut) ;
-router.get("/" , getAllLockout) ;
-router.get("/:LockOutId" , getOneLockOut) ;
-export default router ;
